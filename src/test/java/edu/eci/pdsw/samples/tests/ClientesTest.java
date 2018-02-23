e7e3e91a7df3635e5736ce0c2035931cb1aab644
nicolaspatino <nicolas.patino@mail.escuelaing.edu.co>
2018-02-22 20:29:17 -0500

    clientesTest

--- src/test/java/edu/eci/pdsw/samples/tests/ClientesTest.java
+++ src/test/java/edu/eci/pdsw/samples/tests/ClientesTest.java
@@ -5,16 +5,21 @@
  */
 package edu.eci.pdsw.samples.tests;
 
+import edu.eci.pdsw.samples.entities.Cliente;
 import edu.eci.pdsw.samples.services.ExcepcionServiciosAlquiler;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
+ * CE1: cliente ya registrado
+ * CE2: cliente nulo
+ * CE3: cliente  valido para registrar
  *
  * 
  */
-public class ClientesTest {
+
+public  class ClientesTest {
 
     public ClientesTest() {
     }
@@ -27,6 +32,26 @@ public class ClientesTest {
     @Test
     public void additems1() throws ExcepcionServiciosAlquiler{
     	
+    }
+        /**
+     * @obj registrar un cliente en el sistema
+     * @param p el nuevo cliente
+     * @pre p!=null
+     * @pos el cliente queda disponible para futuros alquileres
+     * @throws ExcepcionServiciosAlquiler si el cliente ya se encuentra registrado
+     */
+    ///public abstract void registrarCliente(Cliente p) throws ExcepcionServiciosAlquiler;
+    
+    @Test
+    public void registrarClienteTest(){       
+        String nombre = "carlos";
+        long documento =2112712;
+        String telefono ="1234567";
+        String direccion = "calle falsa 123";
+        String email ="carlos@invento.com";
+        Cliente cliente= new Cliente(nombre,documento,telefono,direccion,email);
+        Cliente p = null;
+        assertEquals(cliente.getDocumento(),documento);
     }
     
     
